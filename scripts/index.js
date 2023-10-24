8238996164e70a4ffc3e17a90d8ff2999277d663
Alex Fang <106351391+alexfang324@users.noreply.github.com>
2023-10-24 16:22:12 -0700

    saving checkpoint

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,26 @@
+'use strict';
 const game = {
+    isRunning: false,
+    curScreenId: 'splash-screen',
     $splashScreenElement: $('#splash-screen'),
     $gameScreenElement: $('#game-screen'),
     $startGameBtn: $('#start-game-btn'),
+    $pauseGameBtn: $('#pause-game-btn'),
+    $endGameBtn:$('end-game-btn'),
     startGame: function(){
         game.$splashScreenElement.toggle();
         game.$gameScreenElement.toggle();
+        game.isRunning = true;
+    },
+    pauseGame: function(){
+        const btnText = game.$pauseGameBtn.text();
+        btnText=='Pause'?game.$pauseGameBtn.text('Resume'):game.$pauseGameBtn.text('Pause');
+        //toggling game running state
+        game.isRunning = !game.isRunning;
     }
 }
 
 $(()=>{
     game.$startGameBtn.on('click',game.startGame);
+    game.$pauseGameBtn.on('click',game.pauseGame);
 })
