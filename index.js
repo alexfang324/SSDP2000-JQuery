0048a97ccbdad423d3bb8fdbef4f21d0fffe7148
Alex Fang <106351391+alexfang324@users.noreply.github.com>
2023-10-30 11:32:50 -0700

    refactored version

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
-'use strict';
+"use strict";
 const game = {
   isRunning: false,
-  wasRunning:false,
-  screenId: 'splash-screen',
-  $gameElement: $('#game'),
-  $gameBodyElement: $('#game-body'),
-  $splashScreenElement: $('#splash-screen'),
-  $gameScreenElement: $('#game-screen'),
-  $startBtn: $('#start-btn'),
-  $pauseBtn: $('#pause-btn'),
-  $endBtn: $('#end-btn'),
-  $helpBtn: $('#help-btn'),
-  $quitBtn: $('#quit-btn'),
-  $playAgainBtn: $('#play-again-btn'),
-  $gameOverQuitBtn: $('#game-over-quit-btn'),
+  wasRunning: false,
+  screenId: "splash-screen",
+  $gameElement: $("#game"),
+  $gameBodyElement: $("#game-body"),
+  $splashScreenElement: $("#splash-screen"),
+  $gameScreenElement: $("#game-screen"),
+  $startBtn: $("#start-btn"),
+  $pauseBtn: $("#pause-btn"),
+  $endBtn: $("#end-btn"),
+  $helpBtn: $("#help-btn"),
+  $quitBtn: $("#quit-btn"),
+  $playAgainBtn: $("#play-again-btn"),
+  $gameOverQuitBtn: $("#game-over-quit-btn"),
   startGame: function () {
-    game.isRunning = true;
-    game.setScreen('game-screen');
-    game.setGameBackground();
+    game.setScreen("game-screen");
+    game.toggleRunning();
   },
   endGame: function () {
-    game.isRunning = false;
-    game.setScreen('game-over-screen');
-    game.setGameBackground();
+    game.setScreen("game-over-screen");
+    game.toggleRunning();
   },
   quitGame: function () {
-    game.isRunning = false;
-    game.setScreen('splash-screen');
-    game.setGameBackground();
+    game.setScreen("splash-screen");
+    game.toggleRunning();
   },
 
   //toggle game state when clicking Pause/Resume button
   toggleRunning: function () {
-    //toggle pause button text
-    const btnText = game.$pauseBtn.text();
-    btnText == 'Pause'
-      ? game.$pauseBtn.text('Resume')
-      : game.$pauseBtn.text('Pause');
-    //update wasRunning and toggle game running state
-    game.wasRunning = game.isRunning;
-    game.isRunning = !game.isRunning;
-
+    switch (game.screenId) {
+      case "splash-screen":
+        game.isRunning = false;
+        break;
+      case "game-screen":
+        game.wasRunning = game.isRunning;
+        game.isRunning = !game.isRunning;
+        //toggle pause button text
+        const btnText = game.$pauseBtn.text();
+        btnText == "Pause"
+          ? game.$pauseBtn.text("Resume")
+          : game.$pauseBtn.text("Pause");
+        break;
+      case "game-over-screen":
+        game.isRunning = false;
+        break;
+    }
     //toggle game background and border
     game.setGameBackground();
   },
@@ -51,17 +56,17 @@ const game = {
     game.$gameBodyElement.children().hide();
     $(`#${screenId}`).show();
 
-    screenId == 'game-screen' ? $('#quit-btn').show() : $('#quit-btn').hide();
-    screenId == 'game-over-screen'
-      ? $('#help-btn').hide()
-      : $('#help-btn').show();
+    screenId == "game-screen" ? $("#quit-btn").show() : $("#quit-btn").hide();
+    screenId == "game-over-screen"
+      ? $("#help-btn").hide()
+      : $("#help-btn").show();
   },
 
   //Decide which instruction to show in modal
   getInstruction: function () {
-    game.screenId == 'game-screen'
-      ? $('#gameplay-modal').modal('show')
-      : $('#setup-modal').modal('show');
+    game.screenId == "game-screen"
+      ? $("#gameplay-modal").modal("show")
+      : $("#setup-modal").modal("show");
     game.wasRunning = game.isRunning;
     game.isRunning = false;
     game.setGameBackground();
@@ -69,37 +74,34 @@ const game = {
 
   //Sets game back to game on mode if it was on when opening modal
   onModalClosed: function () {
-    if (game.screenId == 'game-screen' && game.wasRunning) {
+    if (game.screenId == "game-screen" && game.wasRunning) {
       game.isRunning = true;
     }
     game.setGameBackground();
   },
-  
+
   //Sets background and border style based on game state
   setGameBackground: function () {
     if (game.isRunning) {
-      game.$gameElement.css('border', 'dashed 5px darkgreen');
-      game.$gameBodyElement.css(
-        'background-image',
-        'url("./game-background.png")'
-      );
+      game.$gameElement.addClass('active');
+      game.$gameBodyElement.addClass('active');
     } else {
-      game.$gameElement.css('border', '');
-      game.$gameBodyElement.css('background-image', '');
+      game.$gameElement.removeClass('active');
+      game.$gameBodyElement.removeClass('active');
     }
-  }
+  },
 };
 
 //Add event listeners on DOM loaded
 $(() => {
-  game.$startBtn.on('click', game.startGame);
-  game.$pauseBtn.on('click', game.toggleRunning);
-  game.$endBtn.on('click', game.endGame);
-  game.$helpBtn.on('click', game.getInstruction);
-  game.$quitBtn.on('click', game.quitGame);
-  game.$playAgainBtn.on('click', game.startGame);
-  game.$gameOverQuitBtn.on('click', () => {
-    game.setScreen('splash-screen');
+  game.$startBtn.on("click", game.startGame);
+  game.$pauseBtn.on("click", game.toggleRunning);
+  game.$endBtn.on("click", game.endGame);
+  game.$helpBtn.on("click", game.getInstruction);
+  game.$quitBtn.on("click", game.quitGame);
+  game.$playAgainBtn.on("click", game.startGame);
+  game.$gameOverQuitBtn.on("click", () => {
+    game.setScreen("splash-screen");
   });
-  $('#gameplay-modal').on('hidden.bs.modal', game.onModalClosed)
+  $("#gameplay-modal").on("hidden.bs.modal", game.onModalClosed);
 });
